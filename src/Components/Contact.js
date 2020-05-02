b5b50d4f9cf6e3c86a1640bb8aa80b69b0da75d3
andruraj <andrewraj10@gmail.com>
2020-05-02 19:32:51 +0530

    contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,6 +2,13 @@ import React from "react";
 
 const Contact = () => {
   const [view, setView] = React.useState(false);
+  const [form, setForm] = React.useState({
+    username: "",
+    email: "",
+    mobile: "",
+    message: "",
+  });
+
   React.useEffect(() => {
     window.addEventListener("scroll", () => {
       const scroll = window.scrollY;
@@ -34,7 +41,48 @@ const Contact = () => {
             </div>
           </span>
         </div>
-        <span className="form">b</span>
+        <div className="form">
+          <div className="identity">
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={form.username}
+              onChange={(e) => setForm({ ...form, username: e.target.value })}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={form.email}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+            <input
+              type="text"
+              pattern="\d*"
+              maxLength="10"
+              name="mobile"
+              placeholder="Mobile Number"
+              value={form.mobile}
+              onChange={(e) => {
+                setForm({ ...form, mobile: e.target.value });
+              }}
+            />
+          </div>
+          <div className="message">
+            <textarea
+              name="message"
+              cols="80"
+              rows="10"
+              placeholder="Enter your message here..."
+              value={form.message}
+              onChange={(e) => setForm({ ...form, message: e.target.value })}
+            ></textarea>
+          </div>
+          <div className="button-anim">
+            <button>Send Message</button>
+          </div>
+        </div>
       </div>
     </div>
   );

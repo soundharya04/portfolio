392fd7dcb50e9555bdf2c7187c96a075850c63d1
soundharya04 <souns14@gmail.com>
2020-05-07 09:09:09 +0530

    contacts

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -4,8 +4,25 @@ import { Vsection } from "./Sections";
 const Contacts = () => {
   return (
     <div>
-      <Vsection classname="vsection" heading="CONTACT"></Vsection>
-      <h2>Footer</h2>
+      <Vsection classname="vsection" heading="CONTACT">
+        <div className="info_box">
+          <div className="info">
+            <div className="info_sec">
+              <h5>Soundharya Kannan</h5>
+              <p>Full stack developer</p>
+            </div>
+            <div className="info_sec">
+              <h5>CALL ME</h5>
+              <p>+91 9677935225</p>
+            </div>
+            <div className="info_sec">
+              <h5>EMAIL</h5>
+              <p>souns14@gmail.com</p>
+            </div>
+            <p></p>
+          </div>
+        </div>
+      </Vsection>
     </div>
   );
 };
--- src/Components/Sections.js
+++ src/Components/Sections.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export const Vsection = ({ classname, heading }) => {
+export const Vsection = ({ classname, heading, ...props }) => {
   return (
     <section className={classname}>
       <h2 className="section_heading">{heading}</h2>
+      <div>{props.children}</div>
     </section>
   );
 };

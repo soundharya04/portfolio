5de75d7709ec0f57f440a1a2cc9038adb02e5bf4
soundharya04 <souns14@gmail.com>
2020-05-07 13:18:03 +0530

    contacts updated

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -3,29 +3,6 @@ import { Vsection } from "./Sections";
 
 const Contacts = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <Vsection classname="vsection" heading="CONTACT">
-        <div className="info_box">
-          <div className="info">
-            <div className="info_sec">
-              <h5>Soundharya Kannan</h5>
-              <p>Full stack developer</p>
-            </div>
-            <div className="info_sec">
-              <h5>CALL ME</h5>
-              <p>+91 9677935225</p>
-            </div>
-            <div className="info_sec">
-              <h5>EMAIL</h5>
-              <p>souns14@gmail.com</p>
-            </div>
-            <p></p>
-          </div>
-        </div>
-      </Vsection>
-    </div>
-=======
     <Vsection classname="vsection" heading="CONTACT">
       <div className="contact">
         <div className="button draw">
@@ -49,7 +26,6 @@ const Contacts = () => {
         </div>
       </div>
     </Vsection>
->>>>>>> 5ad99d148e757a5659c23b791794188084815c4c
   );
 };
 
--- src/Components/Sections.js
+++ src/Components/Sections.js
@@ -3,10 +3,7 @@ import React from "react";
 export const Vsection = ({ classname, heading, ...props }) => {
   return (
     <section className={classname}>
-<<<<<<< HEAD
-=======
       {console.log(classname, "123")}
->>>>>>> 5ad99d148e757a5659c23b791794188084815c4c
       <h2 className="section_heading">{heading}</h2>
       <div>{props.children}</div>
     </section>
